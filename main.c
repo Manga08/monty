c4b19fdd9b8d9ae895d00e296aae9271a2736738
juandsuarezz <1486@holbertonschool.com>
2020-05-12 21:05:35 +0000

    [Fixed]: Main errors

--- main.c
+++ main.c
@@ -1,33 +1,35 @@
 #include "monty.h"
 /**
- * main - Main function
- * @argc: Number of the arguments
- * @argv: Arguments Received
- * Return: 0 or Exit on failure
- */
-
-int main(int agrc, char *argv[])
-{
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	entefile(argv[1]);
-	return (0);
-}
-
-/**
  * enterfile - Enter and read the monty file
  * @montyfile: Monty file
  */
 
 void enterfile(char *montyfile)
 {
+	FILE *file;
 	file = fopen(montyfile, "r");
 	if (file == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", montyfile);
 		exit(EXIT_FAILURE);
 	}
+	fclose(file);
+}
+
+/**
+ * main - Main function
+ * @argc: Number of the arguments
+ * @argv: Arguments Received
+ * Return: 0 or Exit on failure
+ */
+
+int main(int argc, char *argv[])
+{
+        if (argc != 2)
+        {
+                fprintf(stderr, "USAGE: monty file\n");
+                exit(EXIT_FAILURE);
+        }
+        enterfile(argv[1]);
+        return (0);
 }
