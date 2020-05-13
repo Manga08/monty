710e8ded697382d630e2c368c745582473d1e59a
juandsuarezz <1486@holbertonschool.com>
2020-05-13 20:44:09 +0000

    [Fix]: Getline loop

--- main.c
+++ main.c
@@ -59,7 +59,9 @@ void enterfile(char *montyfile, char *buffer, size_t size)
 	{
 		line_num++;
 		tokens = tokenize(buffer);
-		if (tokens != NULL)
+		if (tokens == NULL)
+			continue;
+		else
 			get_op(tokens, line_num)(&stack, line_num);
 	}
 	fclose(file);
